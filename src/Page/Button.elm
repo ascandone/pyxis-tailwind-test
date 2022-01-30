e4d8548883f6e98476d841b350f5db9e51813913
ascandone <alescandone@gmail.com>
2022-01-30 15:37:55 +0100

    extracted let

--- src/Page/Button.elm
+++ src/Page/Button.elm
@@ -7,34 +7,34 @@ import Html.Attributes
 import Section exposing (Section)
 
 
+makeGroup : (List (Button.Attribute msg) -> String -> Html.Html msg) -> List (Button.Attribute msg) -> Html.Html msg
+makeGroup constructor attrs =
+    Html.div [ Html.Attributes.class "flex gap-x-8 gap-y-4 items-center flex-wrap" ]
+        [ constructor (Button.size Button.small :: attrs) "Text"
+        , constructor (Button.size Button.medium :: attrs) "Text"
+        , constructor (Button.size Button.large :: attrs) "Text"
+        , constructor (Button.size Button.huge :: attrs) "Text"
+        ]
+
+
+makeSection : (List (Button.Attribute msg) -> String -> Html.Html msg) -> String -> Section msg
+makeSection constructor header =
+    let
+        makeGroup_ =
+            makeGroup constructor
+    in
+    Section.section header
+        [ makeGroup_ []
+        , makeGroup_ [ Button.contentWidth True ]
+        , makeGroup_ [ Button.loading True ]
+        , makeGroup_ [ Button.icon Button.leadingPlacement FeatherIcons.edit2 ]
+        , makeGroup_ [ Button.icon Button.trailingPlacement FeatherIcons.edit2 ]
+        , makeGroup_ [ Button.icon Button.only FeatherIcons.edit2 ]
+        ]
+
+
 view : List (Section msg)
 view =
-    let
-        btnGroup =
-            Html.div [ Html.Attributes.class "flex gap-x-8 gap-y-4 items-center flex-wrap" ]
-
-        makeGroup constructor attrs =
-            btnGroup
-                [ constructor (Button.size Button.small :: attrs) "Text"
-                , constructor (Button.size Button.medium :: attrs) "Text"
-                , constructor (Button.size Button.large :: attrs) "Text"
-                , constructor (Button.size Button.huge :: attrs) "Text"
-                ]
-
-        makeSection constructor header =
-            let
-                makeGroup_ =
-                    makeGroup constructor
-            in
-            Section.section header
-                [ makeGroup_ []
-                , makeGroup_ [ Button.contentWidth True ]
-                , makeGroup_ [ Button.loading True ]
-                , makeGroup_ [ Button.icon Button.leadingPlacement FeatherIcons.edit2 ]
-                , makeGroup_ [ Button.icon Button.trailingPlacement FeatherIcons.edit2 ]
-                , makeGroup_ [ Button.icon Button.only FeatherIcons.edit2 ]
-                ]
-    in
     [ makeSection Button.primary "Primary"
     , makeSection Button.secondary "Secondary"
     , makeSection Button.tertiary "Tertiary"
