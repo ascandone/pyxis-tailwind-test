b43b92eed8c81fa8dc6e18728fed193e32f68c65
ascandone <alescandone@gmail.com>
2022-01-30 15:36:00 +0100

    generated all combinations

--- src/Page/Button.elm
+++ src/Page/Button.elm
@@ -1,6 +1,7 @@
 module Page.Button exposing (view)
 
 import Components.Button as Button
+import FeatherIcons
 import Html
 import Html.Attributes
 import Section exposing (Section)
@@ -10,67 +11,33 @@ view : List (Section msg)
 view =
     let
         btnGroup =
-            Html.div [ Html.Attributes.class "flex gap-x-8 gap-y-3 items-start flex-wrap" ]
+            Html.div [ Html.Attributes.class "flex gap-x-8 gap-y-4 items-center flex-wrap" ]
+
+        makeGroup constructor attrs =
+            btnGroup
+                [ constructor (Button.size Button.small :: attrs) "Text"
+                , constructor (Button.size Button.medium :: attrs) "Text"
+                , constructor (Button.size Button.large :: attrs) "Text"
+                , constructor (Button.size Button.huge :: attrs) "Text"
+                ]
+
+        makeSection constructor header =
+            let
+                makeGroup_ =
+                    makeGroup constructor
+            in
+            Section.section header
+                [ makeGroup_ []
+                , makeGroup_ [ Button.contentWidth True ]
+                , makeGroup_ [ Button.loading True ]
+                , makeGroup_ [ Button.icon Button.leadingPlacement FeatherIcons.edit2 ]
+                , makeGroup_ [ Button.icon Button.trailingPlacement FeatherIcons.edit2 ]
+                , makeGroup_ [ Button.icon Button.only FeatherIcons.edit2 ]
+                ]
     in
-    [ Section.section "Primary"
-        [ btnGroup
-            [ Button.primary [ Button.size Button.small ] "Text"
-            , Button.primary [ Button.size Button.medium ] "Text"
-            , Button.primary [ Button.size Button.large ] "Text"
-            , Button.primary [ Button.size Button.huge ] "Text"
-            , Button.primary [ Button.loading True ] "Text"
-            ]
-        , btnGroup
-            [ Button.primary [ Button.loading True ] "Text"
-            ]
-        ]
-    , Section.section "Secondary"
-        [ btnGroup
-            [ Button.secondary [ Button.size Button.small ] "Text"
-            , Button.secondary [ Button.size Button.medium ] "Text"
-            , Button.secondary [ Button.size Button.large ] "Text"
-            , Button.secondary [ Button.size Button.huge ] "Text"
-            ]
-        , btnGroup
-            [ Button.secondary [ Button.loading True ] "Text"
-            ]
-        ]
-    , Section.section "Tertiary"
-        [ btnGroup
-            [ Button.tertiary [ Button.size Button.small ] "Text"
-            , Button.tertiary [ Button.size Button.medium ] "Text"
-            , Button.tertiary [ Button.size Button.large ] "Text"
-            , Button.tertiary [ Button.size Button.huge ] "Text"
-            ]
-        , btnGroup
-            [ Button.tertiary [ Button.loading True ] "Text"
-            ]
-        ]
-    , Section.section "Brand"
-        [ btnGroup
-            [ Button.brand [ Button.size Button.small ] "Text"
-            , Button.brand [ Button.size Button.medium ] "Text"
-            , Button.brand [ Button.size Button.large ] "Text"
-            , Button.brand [ Button.size Button.huge ] "Text"
-            ]
-        , btnGroup
-            [ Button.brand [ Button.loading True ] "Text"
-            ]
-        ]
-    , Section.section "Ghost"
-        [ btnGroup
-            [ Button.ghost [ Button.size Button.small ] "Text"
-            , Button.ghost [ Button.size Button.medium ] "Text"
-            , Button.ghost [ Button.size Button.large ] "Text"
-            , Button.ghost [ Button.size Button.huge ] "Text"
-            ]
-        ]
-    , Section.section "Loading"
-        [ btnGroup
-            [ Button.primary [ Button.loading True, Button.size Button.small ] "Text"
-            , Button.primary [ Button.loading True, Button.size Button.medium ] "Text"
-            , Button.primary [ Button.loading True, Button.size Button.large ] "Text"
-            , Button.primary [ Button.loading True, Button.size Button.huge ] "Text"
-            ]
-        ]
+    [ makeSection Button.primary "Primary"
+    , makeSection Button.secondary "Secondary"
+    , makeSection Button.tertiary "Tertiary"
+    , makeSection Button.brand "Brand"
+    , makeSection Button.ghost "Ghost"
     ]
