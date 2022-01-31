3900439edb7cd44c55ece0b7522e6d8c659bc8b3
ascandone <alescandone@gmail.com>
2022-01-31 09:15:40 +0100

    internal minor refactor

--- src/Components/Internal.elm
+++ src/Components/Internal.elm
@@ -17,27 +17,18 @@ formFieldRadiusClass =
 
 formFieldClass : { r | validation : Result error (), disabled : Bool } -> Attribute msg
 formFieldClass { validation, disabled } =
+    let
+        stateClass =
+            Utils.stateClass validation disabled
+    in
     classList
         [ ( "group flex-1 flex border-2 leading-none transition-all", True )
         , ( formFieldRadiusClass, True )
         , ( formFieldTransitionClass, True )
-        , ( case ( validation, disabled ) of
-                ( _, True ) ->
-                    "bg-neutral-100 border-neutral-200 placeholder:text-neutral-400"
-
-                ( Ok (), _ ) ->
-                    "focus-within:border-cyan-600 hover:border-cyan-600 focus-within:ring ring-cyan-200 text-gray-900"
-
-                ( Err _, _ ) ->
-                    "border-red-500  focus-within:ring ring-red-200 text-red-800 placeholder:text-red-200"
-          , True
-          )
-        , ( Utils.stateClass
-                validation
-                disabled
-                { default = "focus-within:border-cyan-600 hover:border-cyan-600 focus-within:ring ring-cyan-200 text-gray-900"
-                , disabled = "bg-neutral-100 border-neutral-200 placeholder:text-neutral-400"
-                , error = "border-red-500  focus-within:ring ring-red-200 text-red-800 placeholder:text-red-200"
+        , ( stateClass
+                { disabled = "bg-neutral-100 border-neutral-200 placeholder:text-neutral-400"
+                , error = "bg-neutral-100 border-neutral-200 placeholder:text-neutral-400"
+                , default = "border-red-500  focus-within:ring ring-red-200 text-red-800 placeholder:text-red-200"
                 }
           , True
           )
